4ad86c91bc298cdfec0ac85450460acbefcd9642
Sullivan Berger <sullivan.berger@yooda.com>
2021-06-13 18:31:53 +0200

    Finished find methods on repositories + reorganizing templates

--- scripts/test.php
+++ scripts/test.php
@@ -7,10 +7,12 @@ use Config\DotEnv;
 
 (new DotEnv(__DIR__ . '/../.env'))->load();
 
-//$db = new MySQLDB(getenv('DATABASE_HOST'), getenv('DATABASE_NAME'), getenv('DATABASE_USER'), getenv('DATABASE_PASSWORD'));
-//
-//$prepared = $db->prepare('select * from posts where user_fk = 5 AND created_at = "2021-02-17 08:53:34" ORDER BY id DESC');
-//$post = $db->execute($prepared, [], Post::class);
-//
-//var_dump($post);
+$db = new MySQLDB(getenv('DATABASE_HOST'), getenv('DATABASE_NAME'), getenv('DATABASE_USER'), getenv('DATABASE_PASSWORD'));
+
+$prepared = $db->prepare('select * from posts where id = :id');
+$post = $db->execute($prepared, [
+    ":id" => 2
+], Post::class);
+
+var_dump($prepared);
 
