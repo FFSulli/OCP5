3d0c96e9b917c89782ac37ac826f512b77610c93
Sullivan Berger <sullivan.berger@yooda.com>
2021-07-14 11:42:33 +0200

    Feat: Handle contact form validation

--- src/Controller/Frontoffice/HomeController.php
+++ src/Controller/Frontoffice/HomeController.php
@@ -2,52 +2,46 @@
 
 namespace App\Controller\Frontoffice;
 
-use App\Model\Entity\ContactMessageModel;
-use App\Model\Entity\Post;
 use App\Model\Repository\PostRepository;
+use App\Service\Form\ContactFormValidator;
 use App\Service\Http\Response;
+use App\Service\Http\Session\Session;
 use App\View\View;
-use http\Client\Request;
+use App\Service\Http\Request;
 
 class HomeController
 {
 
     private View $view;
     private PostRepository $postRepository;
+    private Session $session;
+    private ContactFormValidator $contactFormValidator;
 
-    public function __construct(PostRepository $postRepository, View $view)
+    public function __construct(PostRepository $postRepository, ContactFormValidator $contactFormValidator, View $view, Session $session)
     {
         $this->postRepository = $postRepository;
+        $this->contactFormValidator = $contactFormValidator;
         $this->view = $view;
+        $this->session = $session;
     }
 
-    public function displayHomepageAction(): Response
+    public function displayHomepageAction(Request $request): Response
     {
         $posts = $this->postRepository->findBy([
             "post_status_fk" => 2
         ], null, 3, 0);
 
+        if ($request->getMethod() === 'POST') {
+            if ($this->contactFormValidator->isValid($request->request()->all())) {
+                return new Response('<h1>Formulaire envoyé</h1><h2>faire une redirection vers la page d\'accueil</h2><a href="index.php?action=posts">Page d\'accueil</a><br>', 200);
+            }
+
+            $this->session->addFlashes('error', 'Formulaire mal renseigné');
+        }
+
         return new Response($this->view->render([
             'template' => 'home',
             'data' => ['posts' => $posts],
         ]));
-    }
-
-    public function handleContactFormAction(Request $request)
-    {
-        if ($request->getRequestMethod() === 'POST') {
-            $contactMessageModel = new ContactMessageModel();
-
-            $firstName = $request->getBody()['firstName'];
-
-            var_dump($firstName);
-            return 'Handled submitted data';
-        }
-
-
-        return new Response($this->view->render([
-            'template' => 'home'
-        ]));
-
     }
 }
