5e4bb2bd15b996eef3464a6dd058d6fa54a913de
Sullivan Berger <sullivan.berger@yooda.com>
2021-10-12 14:56:16 +0200

    Feat: Add status code to response

--- src/Service/Http/Response.php
+++ src/Service/Http/Response.php
@@ -24,7 +24,21 @@ class Response
             exit();
         }
 
-//        header();
+        switch ($this->status) {
+            case 200: header("HTTP/1.0 200 OK"); break;
+            case 201: header("HTTP/1.0 201 Created"); break;
+            case 202: header("HTTP/1.0 202 Accepted"); break;
+            case 300: header("HTTP/1.0 300 Multiple Choices"); break;
+            case 301: header("HTTP/1.0 301 Moved Permanently"); break;
+            case 302: header("HTTP/1.0 302 Found"); break;
+            case 400: header("HTTP/1.0 302 Bad Request"); break;
+            case 401: header("HTTP/1.0 401 Unauthorized"); break;
+            case 403: header("HTTP/1.0 403 Forbidden"); break;
+            case 404: header("HTTP/1.0 404 Not Found"); break;
+            case 500: header("HTTP/1.0 500 Internal Server Error"); break;
+            default:
+                exit("Unknown HTTP status code : " . $this->status);
+        }
 
         echo $this->content;
     }
